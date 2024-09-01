e69e071bed3cb0525473df050265ffaaaec0addf
copterbuddy <copterbuddy@gmail.com>
2024-09-01 18:20:40 +0700

    init project

---
+++ reactdotnet.client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import './App.css';
+
+function App() {
+
+    return (
+        <div>
+            <Link to="/weatherforecast">Weather forecast</Link>
+        </div>
+    );
+}
+
+export default App;
